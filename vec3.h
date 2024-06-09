5939ae11a7df2dfab8e89e81e27468e95e0c06dd
koliveiraba <klismann.professional@gmail.com>
2024-06-09 19:41:42 -0300

    added random unit vector

--- vec3.h
+++ vec3.h
@@ -113,10 +113,14 @@ inline vec3 unit_vector(vec3 v) {
 
 vec3 random_in_unit_sphere() {
 	while(true) {
-		auto p = vec3::random(1, -1);
+		auto p = vec3::random(-1, 1);
 		if (p.length_squared() >= 1) continue;
-		return p;
+		return p; 
 	}
+}
+
+vec3 random_unit_vector() {
+	return unit_vector(random_in_unit_sphere());
 }
 
 #endif
